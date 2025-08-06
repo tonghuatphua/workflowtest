2f3e56f9123ce682a7cae43783007656f899207a
tonghuatphua <tonghuat.phua@redalphacyber.com>
2025-08-06 12:55:45 +0800

    Updated assert in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b
 
 def test():
-    assert add(2, 3) == 1, "Test failed: add(2, 3) should be 5"
+    assert add(2, 3) == 5, "Test passed: add(2, 3) should be 5"
 
 
 if __name__ == "__main__":
